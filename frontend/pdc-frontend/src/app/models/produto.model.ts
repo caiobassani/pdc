3c740dece961290760a314e21aa9abf3d95f52bb
caiobassani <caio_bassani@yahoo.com.br>
2017-12-02 18:53:22 -0200

    Criação dos modelos no frontend.

---
+++ frontend/pdc-frontend/src/app/models/produto.model.ts
@@ -0,0 +1,6 @@
+export class Produto {
+    idProduto: number;
+    nome: string;
+    valor: number;
+    qtdEstoque: number;
+}
---
+++ frontend/pdc-frontend/src/app/models/venda.model.ts
@@ -0,0 +1,12 @@
+import { Cliente } from "./cliente.model";
+import { Produto } from "./produto.model";
+import { Filial } from "./filial.model";
+
+export class Venda {
+    idVenda: number;
+    cliente: Cliente;
+    produto: Produto;
+    filial: Filial;
+    qtd: number;
+    valorUnitario: number;
+}
